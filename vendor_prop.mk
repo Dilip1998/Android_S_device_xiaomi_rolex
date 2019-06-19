c15c7243c25412c1abb40d5740ac4eb83a686df9
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-06-19 13:23:14 +0000

    rolex: Re-Enable hardware compositing
    this partially reverts commit a1aa66cc1f0f8b8a5b9780de0fe65f25e9950520
    
    Signed-off-by: sunnyraj84348 <sunnyraj84348@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -100,13 +100,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.egl.hw=0 \
     debug.hwui.use_buffer_age=false \
-    debug.sf.hw=0 \
     debug.egl.hw=1 \
     debug.gralloc.gfx_ubwc_disable=0 \
     debug.hwui.use_buffer_age=false \
     debug.sdm.support_writeback=0 \
+    debug.egl.hw=1 \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    debug.enable.sglscale=1 \
+    debug.hwui.use_buffer_age=false \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
