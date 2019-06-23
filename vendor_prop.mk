1d6ea090d74538a322380474d24282b699822994
karthick111 <karthickrko61@gmail.com>
2019-06-23 17:49:42 +0530

    rolex: Update properties to P
    Change-Id: Ibd2489ad52f09898c8d5f3070054065cdf27fa9e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -100,13 +100,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
-    vendor.gralloc.enable_fb_ubwc=1 \
-    debug.enable.sglscale=1 \
     debug.hwui.use_buffer_age=false \
     debug.sf.hw=0 \
+    debug.egl.hw=1 \
+    debug.gralloc.gfx_ubwc_disable=0 \
+    debug.hwui.use_buffer_age=false \
+    debug.sdm.support_writeback=0 \
+    debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
-    debug.sdm.support_writeback=0 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     persist.hwc.enable_vds=1 \
@@ -115,6 +117,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_skip_validate=1 \
     vendor.display.perf_hint_window=50 \
     debug.hwui.profile=true
+    vendor.gralloc.enable_fb_ubwc=1
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
